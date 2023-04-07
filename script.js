464b9bac928cb776feb0c6764f013b6fe0bc5975
Manuel_Ineichen <113027541+ManuelIni@users.noreply.github.com>
2023-04-07 13:02:22 +0200

    added intro animation

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
 
-function navStart() {
-    letterBox = getElementsByClassName("letterBox");
-    if (screen.width > 769) {
-        letterBox[0].style.animationName = "intro_mobile_left";
-        letterBox[0].style.animationName = "intro_mobile_right";
-        letterBox[0].style.animationName = "intro_mobile_right";
-        letterBox[0].style.animationName = "intro_mobile_left";
-    }
-    else {
+let letterBox;
 
-        for (i=0; i<letterBox.length; i++) {
+window.onload = function () {
+    console.log(window.innerWidth),
+        letterBox = document.getElementsByClassName("letterBox");
+    if (window.innerWidth > 769) {
+        for (i = 0; i < letterBox.length; i++) {
             letterBox[i].style.animationName = "intro_desktop";
             letterBox[i].style.animationPlayState = "running";
+            letterBox[i].addEventListener("animationend", resetPosition, false);
+        }
+    }
+    else {
+        for (i = 0; i < letterBox.length; i++) {
+            letterBox[i].style.animationPlayState = "running";
+            letterBox[i].addEventListener("animationend", resetPosition, false);
+        }
+    }
+}
+
+function resetPosition(event) {
+    if (window.innerWidth > 769) {
+        for (i = 0; i < letterBox.length; i++) {
+            letterBox[i].style.top = null;
+            letterBox[i].classList.replace("introAnimation", "hoverAnimation");
+
+        }
+    }
+    else {
+        for (i = 0; i < letterBox.length; i++) {
+            if (i % 2 == 0) {
+                letterBox[i].style.left = null;
+            }
+            else {
+                letterBox[i].style.right = null;
+            }
+            letterBox[i].classList.replace("introAnimation", "hoverAnimation");
         }
     }
 }
