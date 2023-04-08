28fec3059a55eae13807dc460be82bfe64d381a5
Manuel_Ineichen <113027541+ManuelIni@users.noreply.github.com>
2023-04-08 14:21:03 +0200

    added disc animation

--- script.js
+++ script.js
@@ -1,42 +1,61 @@
 
 let letterBox;
+let firstload;
+
+if (getSessionStorage("firstload") == undefined) {
+    setSessionStorage("firstload",true);
+}
+
+firstload = getSessionStorage("firstload");
 
 window.onload = function () {
-    console.log(window.innerWidth),
+    //if (firstload) {
         letterBox = document.getElementsByClassName("letterBox");
-    if (window.innerWidth > 769) {
-        for (i = 0; i < letterBox.length; i++) {
-            letterBox[i].style.animationName = "intro_desktop";
-            letterBox[i].style.animationPlayState = "running";
-            letterBox[i].addEventListener("animationend", resetPosition, false);
+        if (window.innerWidth > 769) {
+            for (i = 0; i < letterBox.length; i++) {
+                letterBox[i].style.animationName = "intro_desktop";
+                letterBox[i].style.animationPlayState = "running";
+                letterBox[i].addEventListener("animationend", resetPosition, false);
+            }
         }
-    }
-    else {
-        for (i = 0; i < letterBox.length; i++) {
-            letterBox[i].style.animationPlayState = "running";
-            letterBox[i].addEventListener("animationend", resetPosition, false);
+        else {
+            for (i = 0; i < letterBox.length; i++) {
+                letterBox[i].style.animationPlayState = "running";
+                letterBox[i].addEventListener("animationend", resetPosition, false);
+            }
         }
-    }
+
+        setSessionStorage("firstload", false);
+        setSessionStorage("letterBox", letterBox);
+    //}
+
 }
 
 function resetPosition(event) {
     if (window.innerWidth > 769) {
-        for (i = 0; i < letterBox.length; i++) {
-            letterBox[i].style.top = null;
-            letterBox[i].classList.replace("introAnimation", "hoverAnimation");
-
-        }
+        event.target.style.top = null;
+        event.target.classList.replace("introAnimation", "hoverAnimation");
     }
     else {
-        for (i = 0; i < letterBox.length; i++) {
-            if (i % 2 == 0) {
-                letterBox[i].style.left = null;
-            }
-            else {
-                letterBox[i].style.right = null;
-            }
-            letterBox[i].classList.replace("introAnimation", "hoverAnimation");
+        switch(event.target.id) {
+            case "D"||"S":
+                event.target.style.left = null;
+                break;
+            
+            case "I"||"C":
+                event.target.style.left = null;
+                break;
         }
+        event.target.classList.replace("introAnimation", "hoverAnimation");
     }
 }
 
+function setSessionStorage(key, value) {
+    value = JSON.stringify(value);
+    sessionStorage.setItem(key, value);
+}
+
+function getSessionStorage(key) {
+    object_json = sessionStorage.getItem(key);
+    return JSON.parse(object_json);
+}
